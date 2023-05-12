680d2ce186501b12f1ba106d17501e11b4cc522d
Jody M <jodyamm@hotmail.com>
2023-05-12 10:35:08 +0000

    Add: ground and grogu sprites and level

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,30 +1,52 @@
 // initialize kaboom context
 kaboom({
   global: true,
-  width: 800,
+  fullscreen: true,
   height: 800,
   canvas: document.querySelector("#game"),
   scale: 1,
   debug: true,
-  background: [0, 0, 0, 0],
+  clearColor: [0, 0, 0, 0],
 })
 
 // loads sprite
 loadRoot("assets/");
-// loadSprite("mando", "sprites/Mando1stSprite.png");
-loadSprite("grogu-walking", "sprites/GroguWalkingSprite.gif");
+loadSprite("mando", "sprites/Mando1stSprite.png");
+loadSprite("ground", "sprites/ground.png");
 
-// const mando = add([
-//   sprite("mando"),
-//   pos(20, 0),
-//   scale(2),
-// ]);
+loadSprite("grogu-transit", "sprites/grogu-transit.png");
 
-const grogu = add([
-  sprite("grogu-walking"),
-  pos(0, 20),
+const mando = add([
+  sprite("mando"),
+  pos(1210, 159),
+  scale(1),
+]);
+
+const ground = add([
+  sprite("ground"),
+  pos(25, 195),
   scale(2),
 ]);
+
+const grogu = add([
+  sprite("grogu-transit"),
+  pos(0, 130),
+  scale(0.5),
+]);
+
+addLevel([
+  '      ',
+  '  @   ',
+  '      ',
+  'xxxxxxxxxx',
+  'xxxxxxxxxx',
+  'xxxxxxxxxx',
+], {
+  width: 40,
+  height: 40,
+  'x': [sprite('ground')]
+});
+
 
 // create game scenes
 scene("game", () => {
@@ -33,7 +55,8 @@ scene("game", () => {
   //layers
   layers(['bg', 'obj', 'ui'], 'obj')
 
-  add([sprite('grogu-walking'), layer('bg')])
+  // add([sprite('mando'), layer('ui')])
+  add([layer('obj'), solid(), 'ground']);
 
 
 })
