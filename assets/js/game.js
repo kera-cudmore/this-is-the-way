cd665b938445f52ec23e3316935a8e1836806673
SooryaGeorge7 <>
2023-05-12 22:10:13 +0000

    added score container

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,15 @@
+
 // initialize kaboom context
 kaboom({
-  global: true,
-  fullscreen: true,
-  height: 800,
-  canvas: document.querySelector("#game"),
-  scale: 1,
-  debug: true,
-  background: [0, 0, 0, 0],
+    global: true,
+    fullscreen: true,
+    height: 800,
+    canvas: document.querySelector("#game"),
+    scale: 1,
+    debug: true,
+    background: [0, 0, 0, 0],
 })
+
 
 // loads sprite
 loadRoot("assets/");
@@ -20,123 +22,125 @@ loadSound("theme", "sounds/FluffingaDuck.mp3");
 
 
 const mando = add([
-  sprite("mando"),
-  pos(1210, 250),
-  scale(1),
+    sprite("mando"),
+    pos(1210, 250),
+    scale(1),
 ]);
 
 
 
 
 function shoot(obj) {
-  const p = add([
-    scale(0.5),
-    sprite(obj.sprite),
-    pos(obj.pos),
-    origin('center'),
-    lifespan(1.5),
-    'projectile'
-  ]);
+    const p = add([
+        scale(0.5),
+        sprite(obj.sprite),
+        pos(obj.pos),
+        origin('center'),
+        lifespan(1.5),
+        'projectile'
+    ]);
 
-  const speed = obj.speed ?? 600;
-  const angle = obj.angle ?? 0;
-  const vx = speed * Math.cos(angle);
-  const vy = speed * Math.sin(angle);
+    const speed = obj.speed ?? 600;
+    const angle = obj.angle ?? 0;
+    const vx = speed * Math.cos(angle);
+    const vy = speed * Math.sin(angle);
 
-  p.action(() => {
-    p.move(vx * dt(), vy * dt());
-  });
+    p.action(() => {
+        p.move(vx * dt(), vy * dt());
+    });
 
-  return p;
+    return p;
 }
 
 
 
 // create game scenes
 scene("game", () => {
-  play("theme", { loop: true });
-  volume(0.1);
+    play("theme", { loop: true });
+    volume(0.1);
 
-  const mando = add([
-    sprite("mando"),
-    pos(1210, 250),
-    scale(1),
-  ]);
-
-
-
-  const grogu = add([
-    sprite("grogu-transit"),
-    pos(0, 0),
-    scale(0.4),
-    body(),
-    area(),
-  ]);
-
-  const movementSpeed = 1000;
-
-  keyDown("up", () => {
-    grogu.jump(300);
-  });
-
-  keyDown("down", () => {
-    grogu.move(0, movementSpeed);
-  });
-
-  keyDown("left", () => {
-    grogu.move(-movementSpeed, 0);
-  });
-
-  keyDown("right", () => {
-    grogu.move(movementSpeed, 0);
-  });
+    const mando = add([
+        sprite("mando"),
+        pos(1210, 250),
+        scale(1),
+    ]);
 
 
 
-  keyPress("space", () => {
-    shoot({
-      sprite: "force",
-      speed: 500,
-      angle: grogu.angle,
-      pos: grogu.pos.add(grogu.width / 2, grogu.height / 2),
+    const grogu = add([
+        sprite("grogu-transit"),
+        pos(0, 0),
+        scale(0.4),
+        body(),
+        area(),
+    ]);
+
+    
+
+    const movementSpeed = 1000;
+
+    keyDown("up", () => {
+        grogu.jump(300);
     });
-  });
-  
 
-  //layers
-  layers(['bg', 'obj', 'ui'], 'obj')
+    keyDown("down", () => {
+        grogu.move(0, movementSpeed);
+    });
 
-  add([sprite, layer("obj")]);
-  addLevel([
-    "                           ",
-    "                           ",
-    "                      =    ",
-    "         ====         =    ",
-    "                      =    ",
-    "               =      =    ",
-    "===========================",
-  ], {
-    // define the size of each block
-    width: 40,
-    height: 40,
-    "=": () => [
-      sprite("ground"),
-      area(),
-      solid(),
-      pos(5, 340),
-      scale(1.2, 1.5),
-      layer("obj"),
-      fixed(),
-    ],
+    keyDown("left", () => {
+        grogu.move(-movementSpeed, 0);
+    });
 
-  })
+    keyDown("right", () => {
+        grogu.move(movementSpeed, 0);
+    });
 
 
 
-  //layers
-  layers(['bg', 'obj', 'ui'], 'obj')
+    keyPress("space", () => {
+        shoot({
+            sprite: "force",
+            speed: 500,
+            angle: grogu.angle,
+            pos: grogu.pos.add(grogu.width / 2, grogu.height / 2),
+        });
+    });
 
-  add([sprite, layer("obj")]);
+
+    //layers
+    layers(['bg', 'obj', 'ui'], 'obj')
+
+    add([sprite, layer("obj")]);
+    addLevel([
+        "                           ",
+        "                           ",
+        "                      =    ",
+        "         ====         =    ",
+        "                      =    ",
+        "               =      =    ",
+        "===========================",
+    ], {
+        // define the size of each block
+        width: 40,
+        height: 40,
+        "=": () => [
+            sprite("ground"),
+            area(),
+            solid(),
+            pos(5, 340),
+            scale(1.2, 1.5),
+            layer("obj"),
+            fixed(),
+        ],
+
+    })
+
+
+
+    //layers
+    layers(['bg', 'obj', 'ui'], 'obj')
+
+    add([sprite, layer("obj")]);
 
 
 })
